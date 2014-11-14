6c83ed2569913efbb154b4c0883f5732b4e0a54b
mvevans <mvevans1@gmail.com>
2014-11-13 20:14:38 -0500

    Paragraph pete complete

--- Project5/script1.js
+++ Project5/script1.js
@@ -5,14 +5,26 @@ $("body").load = $(go);
 var oh = 0;
 var ow = 0;
 function go(){
-	$("#pete").draggable();
+	$("#pete").draggable({start: dragCallBack, stop: endDragCallBack});
 	$("#pete").resizable({start: resizeCallBack, stop: endResizeCallBack});
+	$("body").mousemove(checkDrag);
+}
+
+function checkDrag(){//the pete doesn't always get his draggable back after being dragged, so this helps get it back in case it doesn't work
+	if(!$("#pete").data('draggable')){
+		$("#pete").draggable({start: dragCallBack, stop: endDragCallBack});
+	}
 }
 
 function resizeCallBack(){
 	$("#pete").text("What is this sorcery? Why is Pete changing sizes?");
 	ow = $("#pete").width();
 	oh = $("#pete").height();
+}
+
+function dragCallBack(){
+	$("#pete").text("Please don't do move Paragraph Pete like this, I'm getting motion sickness! I already had a headache from looking at this screen for hours!");
+	$("#pete").css("background-color","green");
 }
 
 function endResizeCallBack(event, ui){
@@ -32,7 +44,20 @@ function endResizeCallBack(event, ui){
 	}
 
 	$("#pete").resizable("destroy");
+	if($("#pete").data('draggable')){
+		$("#pete").draggable("destroy");
+	}
 	$("#pete").text(str);
+	$("#pete").resizable({start: resizeCallBack, stop: endResizeCallBack});
+	$("#pete").draggable({start: dragCallBack, stop: endDragCallBack});
+}
+
+function endDragCallBack(){
+	$("#pete").css("background-color","purple");
+	$("#pete").text("Why did you do that? I'm too many pixels away home.");
+	$("#pete").draggable("destroy");
+	$("#pete").draggable({start: dragCallBack, stop: endDragCallBack});
+	$("#pete").resizable("destroy");
 	$("#pete").resizable({start: resizeCallBack, stop: endResizeCallBack});
 }
 

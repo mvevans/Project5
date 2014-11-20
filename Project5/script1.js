9ccf3f43b0cf860feb29cda4a4c36c94f6f94292
mvevans <mvevans1@gmail.com>
2014-11-20 14:44:29 -0500

    done

--- Project5/script1.js
+++ Project5/script1.js
@@ -5,24 +5,36 @@ $("body").load = $(go);
 var height = 0;
 var width = 0;
 var name = "mvevans";
-var position;
+var offset;
 function go(){
 	$("#pete").draggable({stop: endDragCallBack});
 	$("#pete").resizable({stop: endResizeCallBack});
+	
+	var json =$.parseJSON( $.ajax( "../Project5/Project5/getDataJSON.php?", { type: "GET", data: "name="+name, async: false}).responseText );
+	
+	$("#pete").width(json.width);
+	$("#pete").height(json.height);
+	$("#pete").css({top:json.y, left:json.x});
+	
 	height = $("#pete").height();
 	width = $("#pete").width();
-	position = $("#pete").position();
+	offset = $("#pete").offset();
+	
+	alert("json.x = " + json.x + " : offset.left = "  + offset.left + "\njson.y = "  + json.y + " : offset.top =  "  + offset.top + "\njson.width = "  + json.width + " : width = "  + width + "\njson.height = " + json.height + " : height = "  + height);
 }
 
 function endResizeCallBack(event, ui){
 	height = $("#pete").height();
 	width = $("#pete").width();
-	var json = $.parseJSON('{"name":"'+name+'","width":'+width+',"height":'+height+'}');
-	//alert(json.name + ", " + name + ", "  + json.width + ", "  + width + ", "  + json.height + ", "  + height);
+	$.ajax( "../Project5/Project5/writeDimensions.php", { type: "GET", data: "name="+name+"&width="+width+"&height="+height, success: check } );
 }
 
 function endDragCallBack(){
-	position = $("#pete").position();
-	var json = $.parseJSON('{"name":"'+name+'","x":'+position.left+',"y":'+position.top+'}');
-	//alert(json.name + ", " + name + ", "  + json.x + ", "  + position.left + ", "  + json.y + ", "  + position.top);
+	offset = $("#pete").offset();
+	$.ajax( "../Project5/Project5/writePosition.php", { type: "GET", data: "name="+name+"&x="+offset.left+"&y="+offset.top, success: check } );
+}
+
+function check(){
+	var json =$.parseJSON( $.ajax( "../Project5/Project5/getDataJSON.php?", { type: "GET", data: "name="+name, async: false}).responseText );
+    alert("json.x = " + json.x + " : offset.left = "  + offset.left + "\njson.y = "  + json.y + " : offset.top =  "  + offset.top + "\njson.width = "  + json.width + " : width = "  + width + "\njson.height = " + json.height + " : height = "  + height);
 }
